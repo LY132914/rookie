7c834d4a9801d853670f406575aae50862120887
LY132914 <146366200+LY132914@users.noreply.github.com>
2023-10-07 20:32:54 +0800

    Update 函数(2).py

--- 函数(2).py
+++ 函数(2).py
@@ -1,10 +1,10 @@
 def cnt(l1):
     l= {}
-    for i in range(len(l1)):
-        if l1[i] in l:
-            l[l1[i]]+=1
+    for i in l1:
+        if i in l:
+            l[i]+=1
         else:
-            l[l1[i]]=1
+            l[i]=1
     return l
 num=[1,2,3,3,3,4,4,4,4,4,5,5,6,6,6]
 print(cnt(num))
